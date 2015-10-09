fd4254144eea9e9f69d97b9cd772fb799b575017
Daniel Esteban Almanza Wagner <daniel.almanzawagner@gmail.com>
2015-10-08 20:54:09 -0500

    add JavaScript logic
    añadí un archivo java script q tiene la capacidad de sumar infinidad de
    números

---
+++ suma.js
@@ -0,0 +1,13 @@
+//Declaracion de Variables
+var numero1;
+var numero2;
+var total;
+
+//Asignación de Valores
+numero1 = 15;
+numero2 = 12;
+total = numero1 + numero2;
+
+alert("si el primer numero es " + numero1);
+alert("y el segundo numero es " + numero2);
+alert("entonces el resultado es " + total); 
